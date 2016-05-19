c6f6b08991602fc668af68dc9db6aacbc1e6e39f
lincolnfz <lincolnfz@gmail.com>
2016-05-19 14:05:45 +0800

    增加静态编译宏

--- WrapCef/cefexprot.h
+++ WrapCef/cefexprot.h
@@ -1,21 +1,8 @@
 #ifndef _cefexport_h
 #define _cefexport_h
 
-/*
-#ifdef _USE_MIRAGE_DLL
-#ifdef MIRAGE_EXPORTS
-#define MIRAGE_API extern "C" __declspec(dllexport)
-#define MIRAGE_CLASS __declspec(dllexport)
-#else
-#define MIRAGE_API extern "C" __declspec(dllimport)
-#define MIRAGE_CLASS __declspec(dllimport)
-#endif
-#else
-#define MIRAGE_API
-#define MIRAGE_CLASS
-#endif
-*/
 
+#ifndef _USE_MIRAGE_LIB
 #if defined(WRAPCEF_EXPORTS)
 #  define SHARED_EXPORT_API extern "C" __declspec(dllexport)
 #  define SHARED_EXPORT_CLASS __declspec(dllexport)
@@ -23,5 +10,11 @@
 #  define SHARED_EXPORT_API extern "C" __declspec(dllimport)
 #  define SHARED_EXPORT_CLASS __declspec(dllimport)
 #endif
+#else
+#  define SHARED_EXPORT_API
+#  define SHARED_EXPORT_CLASS
+#endif // !_USE_MIRAGE_LIB
+
+
 
 #endif
